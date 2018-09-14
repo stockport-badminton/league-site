6d6381f352af31982ba3c963df75f296d0c49e4c
stockport-badminton <stockport.badders.results@gmail.com>
2018-09-14 13:24:51 +0100

    auth0 fixes

--- app.js
+++ app.js
@@ -64,43 +64,7 @@
       }
     })
 
-    var session = require('express-session');
 
-    //session-related stuff
-    var sess = {
-     secret: 'CHANGE THIS SECRET',
-     cookie: {},
-     resave: false,
-     saveUninitialized: true
-    };
-
-    if (app.get('env') === 'production') {
-     sess.cookie.secure = true; // serve secure cookies, requires https
-    }
-
-    app.use(session(sess));
-    var Auth0Strategy = require('passport-auth0'),
-    passport = require('passport');
-
-    //passport-auth0
-    var strategy = new Auth0Strategy({
-      domain: process.env.AUTH0_DOMAIN,
-      clientID: process.env.AUTH0_CLIENTID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET, // Replace this with the client secret for your app
-      callbackURL: 'http://localhost:3000/auth0-callback'
-     },
-     function(accessToken, refreshToken, extraParams, profile, done) {
-       // accessToken is the token to call Auth0 API (not needed in the most cases)
-       // extraParams.id_token has the JSON Web Token
-       // profile has all the information from the user
-       return done(null, profile);
-     }
-    );
-
-    passport.use(strategy);
-
-    app.use(passport.initialize());
-    app.use(passport.session());
 
 
     // Require controller modules
@@ -112,14 +76,6 @@
     var game_controller = require(__dirname + '/controllers/gameController');
     var fixture_controller = require(__dirname + '/controllers/fixtureController');
     var league_controller = require(__dirname + '/controllers/leagueController');
-
-    app.use(function(req, res, next) {
-      res.locals.loggedIn = false;
-      if (req.session.passport && typeof req.session.passport.user != 'undefined') {
-        res.locals.loggedIn = true;
-      }
-      next();
-    });
 
 
     app.get('/', function(req, res) {
