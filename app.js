cd71bf687cdc20c9ac9d7013b33326aab665168a
stockport-badminton <stockport.badders.results@gmail.com>
2018-12-13 13:20:28 +0000

    added profanity filter to contact us

--- app.js
+++ app.js
@@ -222,6 +222,22 @@
       }
     }
 
+    function containsProfanity(value,{req}){
+      var substringsArray = ["ahole","anus","ash0le","ash0les","asholes","ass","Ass Monkey","Assface","assh0le","assh0lez","asshole","assholes","assholz","asswipe","azzhole","bassterds","bastard","bastards","bastardz","basterds","basterdz","Biatch","bitch","bitches","Blow Job","boffing","butthole","buttwipe","c0ck","c0cks","c0k","Carpet Muncher","cawk","cawks","Clit","cnts","cntz"," cock","cockhead","cock-head","cocks","CockSucker","cock-sucker","crap","cum","cunt","cunts","cuntz","dick","dild0","dild0s","dildo","dildos","dilld0","dilld0s","dominatricks","dominatrics","dominatrix","dyke","enema","f u c k","f u c k e r","fag","fag1t","faget","fagg1t","faggit","faggot","fagit","fags","fagz","faig","faigs","fart","flipping the bird","fuck","fucker","fuckin","fucking","fucks","Fudge Packer","fuk","Fukah","Fuken","fuker","Fukin","Fukk","Fukkah","Fukken","Fukker","Fukkin","g00k","gay","gayboy","gaygirl","gays","gayz","God-damned","h00r","h0ar","h0re","hells","hoar","hoor","hoore","jackoff","jap","japs","jerk-off","jisim","jiss","jizm","jizz","knob","knobs","knobz","kunt","kunts","kuntz","Lesbian","Lezzian","Lipshits","Lipshitz","masochist","masokist","massterbait","masstrbait","masstrbate","masterbaiter","masterbate","masterbates","Motha Fucker","Motha Fuker","Motha Fukkah","Motha Fukker","Mother Fucker","Mother Fukah","Mother Fuker","Mother Fukkah","Mother Fukker","mother-fucker","Mutha Fucker","Mutha Fukah","Mutha Fuker","Mutha Fukkah","Mutha Fukker","n1gr","nastt","nigger;","nigur;","niiger;","niigr;","orafis","orgasim;","orgasm","orgasum","oriface","orifice","orifiss","packi","packie","packy","paki","pakie","paky","pecker","peeenus","peeenusss","peenus","peinus","pen1s","penas","penis","penis-breath","penus","penuus","Phuc","Phuck","Phuk","Phuker","Phukker","polac","polack","polak","Poonani","pr1c","pr1ck","pr1k","pusse","pussee","pussy","puuke","puuker","queer","queers","queerz","qweers","qweerz","qweir","recktum","rectum","retard","sadist","scank","schlong","screwing","semen","sex","sexy","Sh!t","sh1t","sh1ter","sh1ts","sh1tter","sh1tz","shit","shits","shitter","Shitty","Shity","shitz","Shyt","Shyte","Shytty","Shyty","skanck","skank","skankee","skankey","skanks","Skanky","slut","sluts","Slutty","slutz","son-of-a-bitch","tit","turd","va1jina","vag1na","vagiina","vagina","vaj1na","vajina","vullva","vulva","w0p","wh00r","wh0re","whore","xrated","xxx","b!+ch","bitch","blowjob","clit","arschloch","fuck","shit","ass","asshole","b!tch","b17ch","b1tch","bastard","bi+ch","boiolas","buceta","c0ck","cawk","chink","cipa","clits","cock","cum","cunt","dildo","dirsa","ejakulate","fatass","fcuk","fuk","fux0r","hoer","hore","jism","kawk","l3itch","l3i+ch","lesbian","masturbate","masterbat*","masterbat3","motherfucker","s.o.b.","mofo","nazi","nigga","nigger","nutsack","phuck","pimpis","pusse","pussy","scrotum","sh!t","shemale","shi+","sh!+","slut","smut","teets","tits","boobs","b00bs","teez","testical","testicle","titt","w00se","jackoff","wank","whoar","whore","*damn","*dyke","*fuck*","*shit*","@$$","amcik","andskota","arse*","assrammer","ayir","bi7ch","bitch*","bollock*","breasts","butt-pirate","cabron","cazzo","chraa","chuj","Cock*","cunt*","d4mn","daygo","dego","dick*","dike*","dupa","dziwka","ejackulate","Ekrem*","Ekto","enculer","faen","fag*","fanculo","fanny","feces","feg","Felcher","ficken","fitt*","Flikker","foreskin","Fotze","Fu(*","fuk*","futkretzn","gay","gook","guiena","h0r","h4x0r"," hell ","helvete","hoer*","honkey","Huevon","hui","injun","jizz","kanker*","kike","klootzak","kraut","knulle","kuk","kuksuger","Kurac","kurwa","kusi*","kyrpa*","lesbo","mamhoon","masturbat*","merd*","mibun","monkleigh","mouliewop","muie","mulkku","muschi","nazis","nepesaurio","nigger*","orospu","paska*","perse","picka","pierdol*","pillu*","pimmel","piss*","pizda","poontsee","poop","porn","p0rn","pr0n","preteen","pula","pule","puta","puto","qahbeh","queef*","rautenberg","schaffer","scheiss*","schlampe","schmuck","screw","sh!t*","sharmuta","sharmute","shipal","shiz","skribz","skurwysyn","sphencter","spic","spierdalaj","splooge","suka","b00b*","testicle*","titt*","twat","vittu","wank*","wetback*","wichser","wop*","yed","zabourah"];
+
+      if (substringsArray.some(function(v) { return value.indexOf(v) >= 0; })) {
+        console.log('containsProfanity fail')
+        return false
+      }
+      // if (substringsArray.some(substring=>yourBigString.includes(substring))) {
+
+      // }
+      else{
+        console.log('containsProfanity sucess')
+        return value
+      }
+    }
+
     function differenceOfTwo(value,{req,path}){
         var otherValue = path.replace('away','home')
         if (Math.abs(value - req.body[otherValue]) < 2){
@@ -486,15 +502,40 @@
         });
     });
 
-    app.post('/contact-us', (req, res,next) => {
-      body('contactEmail', 'must enter an email address').not().isEmpty().isEmail();
-      body('contactQuery', 'Please enter something in message field.').not().isEmpty();
-
-      sanitizeBody('contactQuery').escape();
-      sanitizeBody('contactQuery').trim();
-
-      var errors = validationResult(req);
+    function validCaptcha(value,{req}){
       var options = {
+        method:'POST',
+        url:'https://www.google.com/recaptcha/api/siteverify?secret='+ process.env.RECAPTCHA_SECRET +'&response='+value,
+        json:true
+      }
+      request(options,function(err,response,body){
+        if (err){
+          console.log(err)
+          return false
+        }
+        else {
+          if (body.success){
+            console.log('recaptcha sucess')
+            return value
+          }
+          else {
+            console.log('recaptcha fail')
+            return false
+          }
+        }
+
+      })
+    }
+
+
+let validateContactUs = [
+  body('contactEmail').not().isEmpty().withMessage('please enter an Email address').isEmail().withMessage('Please enter a valid email address'),
+  body('contactQuery').not().isEmpty().withMessage('Please enter something in message field.').custom(containsProfanity).withMessage("Please don't use profanity in the message body"),
+  body('g-recaptcha-response').not().custom(validCaptcha).withMessage('your not a human')
+]
+    app.post('/contact-us',validateContactUs, (req, res,next) => {
+      var errors = validationResult(req);
+      /* var options = {
         method:'POST',
         url:'https://www.google.com/recaptcha/api/siteverify?secret='+ process.env.RECAPTCHA_SECRET +'&response='+req.body['g-recaptcha-response'],
         json:true
@@ -507,16 +548,18 @@
         }
         else {
           if (body.success){
-            // console.log("got here");
+            // console.log("got here"); */
             if (!errors.isEmpty()) {
                 console.log(errors.array());
                 res.render('beta/contact-us-form-delivered', {
-                  pageTitletitle: 'Contact Us - Error',
+                  pageTitle: 'Contact Us - Error',
                   pageDescription: 'Sorry we weren\'t able sent your email - something went wrong',
+                  message: 'Sorry something went wrong',
                   static_path:'/static',
                   theme:'flatly',
-                  content: errors.array()});
-            return;
+                  content: errors.array()
+                });
+                return;
             }
             else {
               // console.log(req.body);
@@ -687,7 +730,8 @@
                     theme: process.env.THEME || 'flatly',
                     flask_debug: process.env.FLASK_DEBUG || 'false',
                     pageTitle: 'Contact Us - Success',
-                    pageDescription: 'Succes - we\'ve sent an email to your chosen contact for you'
+                    pageDescription: 'Succes - we\'ve sent an email to your chosen contact for you',
+                    message: 'Success - we\'ve sent your email to your chosen contact'
                 });
               })
               .catch(error => {
@@ -711,16 +755,7 @@
                 }
               }); */
             }
-          }
-          else {
-            // console.log("got here instead");
-            return next(" your not a human")
-          }
-        }
-
-      })
-
-    });
+          })
 
     /// PLAYER ROUTES ///
 
