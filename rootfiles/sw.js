a62ea5d298c0cd568e775770a281fb5b8f638e0f
stockport-badminton <stockport.badders.results@gmail.com>
2019-02-24 23:33:52 +0000

    pwa changes

--- rootfiles/sw.js
+++ rootfiles/sw.js
@@ -13,28 +13,9 @@ self.addEventListener('install', function(e) {
       return cache.addAll([
         '/',
         '/messer-rules',
-        '/static/beta/docs/MesserASection1819.pdf',
-        '/static/beta/docs/MesserASection1819Results.pdf',
-        '/static/beta/docs/MesserBSection1819.pdf',
-        '/static/beta/docs/MesserBSection1819Results.pdf',
-        '/static/beta/docs/Messer-Score-Sheet.xlsx',
-        '/static/beta/docs/OpenEntryForm.doc',
-        '/static/beta/docs/OpenLetter.doc',
-        '/static/beta/docs/HandicapClubEntryForm.docx',
-        '/static/beta/docs/HandicapIndividualEntryForm.docx',
-        '/static/beta/docs/OpenResults2018.xlsx',
-        '/static/beta/docs/OpenRoundRobinResults2018.docx',
-        '/static/beta/docs/Scorecard-20182019.xls',
-        '/static/beta/docs/Scorecard-20182019.xlsx',
-        '/static/beta/docs/Scorecard-print.pdf',
-        '/static/beta/docs/SDBL-Junior-League-2019.docx',
-        '/static/beta/docs/stockport-junior-tournament-entry-form-2019.docx',
         '/info/clubs',
         '/rules',
-        '/contact-us',
-        '/static/beta/css/custom.css',
-        '/static/beta/jquery/jquery-1.11.3.min.js',
-        '/static/beta/bootstrap/js/bootstrap.min.js'
+        '/contact-us'
       ]);
     })
   );
