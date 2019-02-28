d60e6516b519f3d9c1e55d31f22a095e939ec061
stockport-badminton <stockport.badders.results@gmail.com>
2019-02-28 21:14:34 +0000

    pwa changes

--- rootfiles/sw.js
+++ rootfiles/sw.js
@@ -1,26 +1,48 @@
-/** An empty service worker! */
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+var CACHE = 'cache-and-update';
+
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+
+  evt.waitUntil(precache());
 });
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('the-magic-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/messer-rules',
-        '/info/clubs',
-        '/rules',
-        '/contact-us',
-        '/static/beta/css/custom.css',
-        '/static/beta/images/bg/1920.png',
-        '/touch-icon-192x192.png',
-        '/favicon.ico'
-      ]);
-    })
-  );
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+
+  evt.waitUntil(update(evt.request));
 });
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      '/',
+      '/messer-rules',
+      '/info/clubs',
+      '/rules',
+      '/contact-us',
+      '/static/beta/css/custom.css',
+      '/static/beta/images/bg/1920.png',
+      '/touch-icon-192x192.png',
+      '/favicon.ico',
+      '/results/All',
+      'tables/All'
+    ]);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
