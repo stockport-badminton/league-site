52aac22b99a080dc6a22dec7db79147d1f0bf0b7
Neil Cooper <neilcooper@Neils-MacBook-Pro.local>
2021-10-20 23:24:27 +0100

    tweaked security

--- models/secured.js
+++ models/secured.js
@@ -1,7 +1,16 @@
 module.exports = function() {
   return function secured (req, res, next) {
-    // console.log(req);
+    console.log(req.user);
     if (req.user) { return next(); }
+    req.session.returnTo = req.originalUrl;
+    res.redirect('/login');
+  };
+};
+
+module.exports = function() {
+  return function restrictResults (req, res, next) {
+    // console.log(req);
+    if (req.user.app_metadata.betaAccess) { return next(); }
     req.session.returnTo = req.originalUrl;
     res.redirect('/login');
   };
