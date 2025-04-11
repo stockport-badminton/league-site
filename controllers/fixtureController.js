ca3128c022263d2d03ddfb6fcdc34b5903d90617
Neil Cooper <stockport.badders.results@gmail.com>
2025-04-11 23:42:51 +0100

    added messer report

--- controllers/fixtureController.js
+++ controllers/fixtureController.js
@@ -791,7 +791,7 @@ exports.fixture_get_summary = function(req, res,next) {
             else{
               var options = {
                 'method': 'GET',
-                'url': 'https://api.cloudinary.com/v1_1/hvunsveuh/resources/image/tags/messer2024?max_results=30&context=true',
+                'url': 'https://api.cloudinary.com/v1_1/hvunsveuh/resources/image/tags/messer2025?max_results=30&context=true',
                 'headers': {
                   'Authorization': 'Basic '+process.env.CLOUDINARY_AUTH
                 }
